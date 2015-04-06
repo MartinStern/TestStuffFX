2055dd509cd121838297ab67a66f2b2ee997fd95
MartinStern <martin.stern.dao@gmail.com>
2015-04-06 16:12:06 +0200

    initial Commit

---
+++ src/teststufffx/CustomTextFieldSample.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teststufffx;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author MartinStern
+ */
+public class CustomTextFieldSample extends Application {
+
+    final static Label label = new Label();
+
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, 300, 150);
+        stage.setScene(scene);
+        stage.setTitle("Text Field Sample");
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+
+        scene.setRoot(grid);
+        final Label dollar = new Label("$");
+        GridPane.setConstraints(dollar, 0, 0);
+        grid.getChildren().add(dollar);
+
+        final TextField sum;
+        sum = new TextField() {
+            @Override
+            public void replaceText(int start, int end, String text) {
+                if (!text.matches("[a-z, A-Z]")) {
+                    label.setText(null);
+                    super.replaceText(start, end, text);
+                } else {
+                    label.setText("Enter a numeric value");
+                }
+            }
+
+            @Override
+            public void replaceSelection(String text) {
+                if (!text.matches("[a-z, A-Z]")) {
+                    super.replaceSelection(text);
+                }
+            }
+        };
+
+        sum.setPromptText("Enter a Number"); //wird beim Start nicht angezeigt, da der Fokus im Feld ist! ausser man setzt den Fokus manuell auf den Button
+        sum.setPrefColumnCount(10);
+        GridPane.setConstraints(sum, 1, 0);
+        grid.getChildren().add(sum);
+
+        Button submit = new Button("Submit");
+        GridPane.setConstraints(submit, 2, 0);
+        grid.getChildren().add(submit);
+
+        submit.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                label.setText("You entered: " + sum.getText());
+                sum.clear();
+            }
+        });
+
+        GridPane.setConstraints(label, 0, 1);
+        GridPane.setColumnSpan(label, 3);
+        grid.getChildren().add(label);
+
+        scene.setRoot(grid);
+        stage.show();
+        submit.requestFocus();  //wir setzen den Fokus auf den Button, damit der Promttext beim Eingabefeld auch beim Start angezeigt wird
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
